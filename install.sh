a1e19cb696e8a259c8d3dd8a037e4da4299d1707
luckypool <luckypool314@gmail.com>
2014-01-07 12:03:39 +0900

    Remove tab

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 cd "$(dirname "$0")"
 for dotfiles in .?*
 do
-	if [ $dotfiles != '..' ] && [ $dotfiles != '.git' ]
-	then
+    if [ $dotfiles != '..' ] && [ $dotfiles != '.git' ]
+    then
         cp $HOME/$dotfiles $HOME/$dotfiles.old
-		ln -Fisv "$PWD/$dotfiles" $HOME
-	fi
+        ln -Fisv "$PWD/$dotfiles" $HOME
+    fi
 done
 
