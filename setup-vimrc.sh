f9cfd7332753f44b336a41b25d3bf6194effe117
luckypool <luckypool314@gmail.com>
2014-01-07 18:42:16 +0900

    Fix setup-vimrc.sh

--- setup-vimrc.sh
+++ setup-vimrc.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-git clone https://github.com/Shougo/neobundle.vim.git $HOME/.vim/bundle/neobundle.vim
-git clone https://gist.github.com/afecd4b6cb37eb24e4c4.git $HOME/.vim/snippets
-git clone https://gist.github.com/5c498d514369dcac962b.git $HOME/.vim/dict
+GIT='sudo -u vagrant git'
+VIM='sudo -u vagrant vim'
 
-for dir in ("$HOME/.vim/backup", "$HOME/.vim/swp")
-do
-    if [ ! -d dir ]; then
-        mkdir dir
+DIRS=".vim .vim/backup .vim/swp .vim/snippets .vim/dict"
+
+for dir in $DIRS; do
+    if [ -d "$HOME/$dir" ]; then
+        rm -rf "$HOME/$dir"
     fi
+    mkdir "$HOME/$dir"
 done
 
-sudo -u vagrant vim \
+$GIT clone https://github.com/Shougo/neobundle.vim.git "$HOME/.vim/bundle/neobundle.vim"
+$GIT clone https://gist.github.com/afecd4b6cb37eb24e4c4.git "$HOME/.vim/snippets"
+$GIT clone https://gist.github.com/5c498d514369dcac962b.git "$HOME/.vim/dict"
+
+$VIM \
     -N \
     -u ~/.vimrc \
     -c "try | NeoBundleUpdate! $* | finally | qall! | endtry" \
