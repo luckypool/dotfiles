99be4cb2f1bfc3efbac8494d2dc71a333c091daa
luckypool <luckypool314@gmail.com>
2014-01-07 12:16:23 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ for dotfiles in .?*
 do
     if [ $dotfiles != '..' ] && [ $dotfiles != '.git' ]
     then
-        cp $HOME/$dotfiles $HOME/$dotfiles.old
+        mv $HOME/$dotfiles $HOME/$dotfiles.old
         ln -Fisv "$PWD/$dotfiles" $HOME
     fi
 done
